45ce227a360ecc5ae05f7b53fcc7e9df0020ae57
lizz <seattlelizzard@gmail.com>
2024-11-04 10:45:18 -0800

    ahhh how do invisible

--- lua/nvim-config.lua
+++ lua/nvim-config.lua
@@ -4,6 +4,11 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.cmd('set background=dark') -- or 'light'
+
+
+vim.cmd('hi Normal ctermbg=none') -- or 'light'
+vim.cmd('highlight LineNr ctermbg=none') -- or 'light'
+vim.cmd('highlight NonText ctermbg=none') -- or 'light'
 vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
 
 vim.cmd('highlight NeoTreeNormal guibg=NONE ctermbg=NONE')
