2bb9162e7d8bfd03a58bd7c14d08cd6a6bd262df
lizz <seattlelizzard@gmail.com>
2024-11-26 16:04:44 -0800

    adding stuff for line numbers

--- lua/nvim-config.lua
+++ lua/nvim-config.lua
@@ -4,7 +4,11 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.cmd('set background=dark') -- or 'light'
-
+vim.cmd('highlight LineNr guifg=#ffffff guibg=#000000')
+vim.cmd('hi LineNrAbove guifg=red ctermfg=red')
+vim.cmd('highlight LineNrBelow guifg=#bdff96 ctermfg=green')
+-- Toggle relative line numbers
+vim.api.nvim_set_keymap('n', '<leader>n', ':set relativenumber!<CR>', { noremap = true, silent = true })
 
 vim.cmd('hi Normal ctermbg=none') -- or 'light'
 vim.cmd('highlight LineNr ctermbg=none') -- or 'light'
