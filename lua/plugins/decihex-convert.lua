08a53587a53be09218d0ec1feb85c3fa480071f9
lizz <seattlelizzard@gmail.com>
2024-11-28 07:41:50 -0800

    Now I can convert a bunch of numbers at once!

--- lua/plugins/decihex-convert.lua
+++ lua/plugins/decihex-convert.lua
@@ -2,8 +2,8 @@ return {
   {
     'nvim-lua/plenary.nvim', -- Optional dependency for shell commands
     config = function()
-      -- Function to replace visual selection with the printf output
-      local function replace_with_printf()
+      -- Function to replace visual selection with the printf output for each number
+      local function replace_numbers_with_hex()
         -- Get the visual selection range
         local line_start, col_start = unpack(vim.fn.getpos("'<"), 2, 3)
         local line_end, col_end = unpack(vim.fn.getpos("'>"), 2, 3)
@@ -16,28 +16,29 @@ return {
           lines[#lines] = string.sub(lines[#lines], 1, col_end)
           lines[1] = string.sub(lines[1], col_start)
         end
-        local selection = table.concat(lines, " ")
+        local selection = table.concat(lines, "\n")
 
-        -- Run printf command on the selection
-        local handle = io.popen(string.format('printf "%%x\\n" %s', selection))
-        local result = handle:read("*a")
-        handle:close()
+        -- Replace each number in the selection
+        local processed = selection:gsub("%d+", function(number)
+          -- Convert the number to hexadecimal
+          local handle = io.popen(string.format('printf "%%x" %s', number))
+          local result = handle:read("*a")
+          handle:close()
+          return result:gsub("%s+$", "") -- Trim trailing spaces/newlines
+        end)
 
-        -- Remove trailing newlines from the result
-        result = result:gsub("%s+$", "")
-
-        -- Replace the selected text with the output
+        -- Replace the selected text with the processed output
         vim.api.nvim_buf_set_text(
           0, -- Current buffer
           line_start - 1, col_start - 1, -- Start position
           line_end - 1, col_end, -- End position
-          { result } -- Replacement text
+          vim.split(processed, "\n", { plain = true }) -- Split into lines
         )
       end
 
-      -- Create a user command for ReplaceWithHex
-      vim.api.nvim_create_user_command("ReplaceWithHex", function()
-        replace_with_printf()
+      -- Create a user command for ReplaceNumbersWithHex
+      vim.api.nvim_create_user_command("ReplaceNumbersWithHex", function()
+        replace_numbers_with_hex()
       end, {
         range = true, -- Allow range for manual invocation
       })
@@ -46,7 +47,7 @@ return {
       vim.api.nvim_set_keymap(
         'v', -- Visual mode
         '<leader>x', -- Your chosen keybinding
-        ":<C-u>ReplaceWithHex<CR>", -- Clear the range before invoking the command
+        ":<C-u>ReplaceNumbersWithHex<CR>", -- Clear the range before invoking the command
         { noremap = true, silent = true }
       )
     end
